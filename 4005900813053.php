f50e62e0120c14d33abc484e661cda915d20ad18
Renato Hirata <renatokhirata@gmail.com>
2022-07-11 17:25:25 -0300

    Squashed commit of the following:
    commit dccd6fbd6fd5dbe577b5fd250e60c1a953d09672
    Merge: 2f56cea 51223b1
    Author: Renato Hirata <renatokhirata@gmail.com>
    Date:   Mon Jul 11 17:11:05 2022 -0300
    
        Merge branch 'main' of https://github.com/renatokhirata/niveascan.com.br
    
    commit 2f56cea1c59a5e5a4a8924df34b4bc7afa6e7040
    Author: Renato Hirata <renatokhirata@gmail.com>
    Date:   Mon Jul 11 17:10:45 2022 -0300
    
        Small change on 4005900813046.php
    
    commit 51223b1bed030fb9047fcbb40a339016d5277346
    Author: renatokhirata <renatokhirata@gmail.com>
    Date:   Mon Jul 11 17:02:18 2022 -0300
    
        Creating deply action
    
    commit 5db667e84a59704a13c709dd8b91269cd1e7ddd2
    Author: claupinheiros <102977963+claupinheiros@users.noreply.github.com>
    Date:   Mon Jul 11 16:38:26 2022 -0300
    
        Add files via upload
    
    commit 93eed9c274cd58ae094a7524bd47344941ce244c
    Author: claupinheiros <102977963+claupinheiros@users.noreply.github.com>
    Date:   Mon Jul 11 16:35:35 2022 -0300
    
        Add files via upload
    
    commit b7ee3c686abcb49c423094fbf12203dd3f4aaa33
    Author: claupinheiros <102977963+claupinheiros@users.noreply.github.com>
    Date:   Mon Jul 11 16:25:59 2022 -0300
    
        Add files via upload
    
    commit 710457f53c04e28e0b1a3a9864a66cb64eb62ea0
    Author: claupinheiros <102977963+claupinheiros@users.noreply.github.com>
    Date:   Mon Jul 11 16:23:39 2022 -0300
    
        Add files via upload
    
    commit 3da1a8f16ba12293cbd80d22dee50c0ed5e0624b
    Author: Renato Hirata <renatokhirata@gmail.com>
    Date:   Fri Jul 8 15:02:04 2022 -0300
    
        Criacao do Pepino

---
+++ 4005900813053.php
@@ -0,0 +1,202 @@
+<?php include "head.php" ?>
+<!-- 4005900813053 -->
+<style>:root {
+	--cor01: #0033a1;
+	--cor02: white;
+	--cor03: #f0deba;
+}</style>
+
+<body>
+	<div class="container-fluid containerIntro">
+		<div class="row justify-content-center d-flex">
+			<div class="col-12 col-md-6">
+
+				<?php include "estrela.php" ?>
+
+				<h1 class="text-center">
+					<span>Muito</span>
+                    <span>   Satisfeiita!</span>
+                    <span>    Parabéns</span>
+                    <span>    pela</span>
+                    <span>   qualidade,</span>
+                    <span>    tenho</span>
+                    <span>    51</span>
+                    <span>    anos</span>
+                    <span>    e</span>
+                    <span>   usando</span>
+                    <span>    este</span>
+                    <span>    produto</span>
+                    <span>    estou</span>
+                    <span>    quase</span>
+                    <span>    zerando</span>
+                    <span>    o</span>
+                    <span>    melasma.</span>
+                    <span>    Muito</span>
+                    <span>    melhor</span>
+                    <span>    e</span>
+                    <span>    mais</span>
+                    <span>    eficiente</span>
+                    <span>    do</span>
+                    <span>    que</span>
+                    <span>    cremes</span>
+                    <span>    caros.</span>
+                    <span>    Além</span>
+                    <span>    disto</span>
+                    <span>    minha</span>
+                    <span>    pele</span>
+                    <span>    continua</span>
+                    <span>    macia</span>
+                    <span>    e</span>
+                    <span>    hidratada</span>
+				
+				</h1>
+				<p class="introAutor text-center">
+				<b>
+					Selma J.
+				</b><br>
+					21/02/2022
+				</p> 
+			</div>
+		</div>
+	</div>
+
+	<div id="second"></div>
+	<div class="container-fluid containerA">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 d-flex justify-content-center">
+				<img src="comp/images/logo-nivea.png" class="nivea-logo" alt="NIVEA">
+			</div>
+			<div class="col-8 col-md-4">
+				<img src="comp/images/4005900813053/4005900813053_00.png" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+			</div>
+			<div class="col-12 col-md-9 text-center">
+				<h1 class="fade-h1 fade-revealUp">
+                Serum Facial Cellular Luminous 630
+					<br><span>30ml</span>
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid containerB">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-12">
+				<div id="prodCarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900813053/4005900813053_3.jpg" alt="NIVEA">
+							</div>
+						</div>
+						<div class="carousel-item">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900813053/4005900813053_4.jpg" alt="NIVEA">
+							</div>
+						</div>
+                        <div class="carousel-item">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900813053/4005900813053_5.jpg" alt="NIVEA">
+							</div>
+						</div>
+                        <div class="carousel-item">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900813053/4005900813053_6.jpg" alt="NIVEA">
+							</div>
+						</div>
+						<div class="carousel-item">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900813053/4005900813053_7.jpg" alt="NIVEA">
+							</div>
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-target="#prodCarousel" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					</button>
+					<button class="carousel-control-next" type="button" data-target="#prodCarousel" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerTexto">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h3 class="fade-revealUp">
+					O que é?
+				</h3>
+				<p class="fade-revealUp">
+                Sérum facial dia e noite da Nivea reduz marcas escurecidas da pele em 4 semanas de uso. Todo cuidado NIVEA para você. Aproveite!
+				</p>
+				<h3 class="fade-revealUp">
+                Para que serve?
+				</h3>
+				<p class="fade-revealUp">
+                Sua fórmula deixa a pele uniforme e com mais luminosidade. Enriquecido com ácido hialurônico e Vitamina E, previne o surgimento de novas manchas.
+				</p>
+				<h3 class="fade-revealUp">
+					Benefícios e diferenciais
+				</h3>
+				<div class="beneficios">
+					<div class="fade-revealUp">
+                    Pele do rosto mais uniforme e livre de manchas
+					</div>
+					<div class="fade-revealUp">
+                    Previne novas manchas
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerC">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 text-center">
+				<h1>Modo de uso</h1>
+				<div class="d-block d-md-none">
+					<div class="modoUsoDiv" style="background-image: url('comp/images/4005900813053/4005900813053_9.jpg')">
+					</div>
+				</div>
+				<div class="d-none d-md-block">
+					<div class="modoUsoDiv-desktop" style="background-image: url('comp/images/4005900813053/4005900813053_9.jpg')">
+					</div>
+				</div>				
+				<h3 id="ingredientes" class="ingredientes" onclick="ingredientesClick()">Ingredientes &#x25BE</h3>
+				<div id="ingredientesLista" class="ingredientesLista">
+					<p>
+                    Aqua, Isopropyl Palmitate, Cetearyl Isononanoate, Dimethicone, Glycerin, Alcohol Denat., Cocoglycerides, Distarch Phosphate, Butylene Glycol, Methylpropanediol, Tapioca Starch, Isobutylamido Thiazolyl Resorcinol, Sodium Hyaluronate, Tocopheryl Acetate, Stearyl Alcohol, Cetyl Alcohol, Glyceryl Stearate, Sodium Ascorbyl Phosphate, Sodium Stearoyl Glutamate, Sodium Polyacrylate, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Trisodium EDTA, Sodium Chloride, Sodium Hydroxide, Sodium Sulfate, Phenoxyethanol, Benzyl Alcohol, Parfum, CI 15985, CI 47005
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerD">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h1 class="fade-revealUp">Especificações</h1>
+				<img src="comp/images/4005900813053/4005900813053_8.jpg" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerE">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-10 text-center">
+				<video width="100%" height="auto" controls autoplay muted loop>
+					<source src="comp/images/4005900813053/4005900813053_video.mp4" type="video/mp4">
+						Your browser does not support the video tag.
+					</video>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid containerF">
+			<div class="row">
+				<div class="col-12">
+					<h1>Compre o seu</h1>
+				</div>
+			</div>
+		</div>
+
+<?php include "foot.php" ?>
