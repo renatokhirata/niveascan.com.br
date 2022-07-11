5db667e84a59704a13c709dd8b91269cd1e7ddd2
claupinheiros <102977963+claupinheiros@users.noreply.github.com>
2022-07-11 16:38:26 -0300

    Add files via upload

---
+++ 4005900894779.php
@@ -0,0 +1,187 @@
+<?php include "head.php" ?>
+<!-- 4005900894779 -->
+<style>:root {
+	--cor01: #0033a1;
+	--cor02: white;
+	--cor03: #ecc1c8;
+}</style>
+
+<body>
+	<div class="container-fluid containerIntro">
+		<div class="row justify-content-center d-flex">
+			<div class="col-12 col-md-6">
+
+				<?php include "estrela.php" ?>
+
+				<h1 class="text-center">
+					<span>Maravilhoso</span>
+                    <span>Passou </span>
+                    <span>a </span>
+                    <span>ser </span>
+                    <span>meu </span>
+                    <span>favorito, </span>
+                    <span>deixa </span>
+                    <span>minha </span>
+                    <span>pele </span>
+                    <span>sequinha </span>
+                    <span>e </span>
+                    <span> hidratada</span> 
+                    <span>o </span>
+                    <span>dia </span>
+                    <span>todo!!</span>
+				
+				</h1>
+				<p class="introAutor text-center">
+				<b>
+                JWalles
+				</b><br>
+					08/06/2022
+				</p> 
+			</div>
+		</div>
+	</div>
+
+	<div id="second"></div>
+	<div class="container-fluid containerA">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 d-flex justify-content-center">
+				<img src="comp/images/logo-nivea.png" class="nivea-logo" alt="NIVEA">
+			</div>
+			<div class="col-8 col-md-4">
+				<img src="comp/images/4005900894779/4005900894779_00.png" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+			</div>
+			<div class="col-12 col-md-9 text-center">
+				<h1 class="fade-h1 fade-revealUp">
+                Creme Hidratante em Gel Aqua Rose
+					<br><span>50ml</span>
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid containerB">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-12">
+				<div id="prodCarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900894779/4005900894779_3.jpg" alt="NIVEA">
+							</div>
+						</div>
+						<div class="carousel-item">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900894779/4005900894779_4.jpg" alt="NIVEA">
+							</div>
+						</div>
+                        <div class="carousel-item">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900894779/4005900894779_5.jpg" alt="NIVEA">
+							</div>
+						</div>
+						<div class="carousel-item">
+							<div class="carousel-imgctn">
+								<img src="comp/images/4005900894779/4005900894779_6.jpg" alt="NIVEA">
+							</div>
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-target="#prodCarousel" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					</button>
+					<button class="carousel-control-next" type="button" data-target="#prodCarousel" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerTexto">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h3 class="fade-revealUp">
+					O que é?
+				</h3>
+				<p class="fade-revealUp">
+                Creme hidratante em gel revitaliza a pele e oferece hidratação por 48h. Para um brilho natural livre de oleosidade. Garanta o seu!
+				</p>
+				<h3 class="fade-revealUp">
+                Para que serve?
+				</h3>
+				<p class="fade-revealUp">
+                Com água de rosas orgânica, poderoso antioxidante, e ácido hialurônico, que mantém a hidratação da pele. Sua textura em gel é rapidamente absorvida e deixa uma sensação de frescor.
+				</p>
+				<h3 class="fade-revealUp">
+					Benefícios e diferenciais
+				</h3>
+				<div class="beneficios">
+					<div class="fade-revealUp">
+                    Com Ácido Hialurônico
+					</div>
+					<div class="fade-revealUp">
+                    Hidrata por 48h
+					</div>
+                    <div class="fade-revealUp">
+                    Não contém álcool etílico
+					</div>
+                    <div class="fade-revealUp">
+                    Dermatologicamente testado 
+					</div>
+                    <div class="fade-revealUp">
+                    Para todos os tipos de pele 
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerC">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 text-center">
+				<h1>Modo de uso</h1>
+				<div class="d-block d-md-none">
+					<div class="modoUsoDiv" style="background-image: url('comp/images/4005900894779/4005900894779_7.jpg')">
+					</div>
+				</div>
+				<div class="d-none d-md-block">
+					<div class="modoUsoDiv-desktop" style="background-image: url('comp/images/4005900894779/4005900894779_7.jpg')">
+					</div>
+				</div>				
+				<h3 id="ingredientes" class="ingredientes" onclick="ingredientesClick()">Ingredientes &#x25BE</h3>
+				<div id="ingredientesLista" class="ingredientesLista">
+					<p>
+                    Aqua, Glycerin, Alcohol Denat., Cetearyl Alcohol, Distarch Phosphate, Dimethicone, Glyceryl Stearate Citrate, 1,2-Hexanediol, Caprylic/Capric Triglyceride, Dibutyl Adipate, Myristyl Myristate, Octyldodecanol, Dicaprylyl Carbonate, Cetearyl Isononanoate, Ammonium Acryloyldimethyltaurate/VP Copolymer, Phenoxyethanol, Carbomer, Trisodium EDTA, Sodium Hydroxide, Rosa Damascena Flower Water, Sodium Hyaluronate, Potassium Sorbate, Sodium Benzoate, Sorbic Acid, Linalool, Geraniol, Citronellol, d-Limonene, Methyl Benzoate, Tocopherol, Parfum, CI 16035
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerD">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h1 class="fade-revealUp">Especificações</h1>
+				<img src="comp/images/4005900894779/4005900894779_8.jpg" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerE">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-10 text-center">
+				<video width="100%" height="auto" controls autoplay muted loop>
+					<source src="comp/images/4005900894779/4005900894779_video.mp4" type="video/mp4">
+						Your browser does not support the video tag.
+					</video>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid containerF">
+			<div class="row">
+				<div class="col-12">
+					<h1>Compre o seu</h1>
+				</div>
+			</div>
+		</div>
+
+<?php include "foot.php" ?>
