0a069e2d4df71b6d8ccd0f731780526d3238085e
Renato Hirata <renatokhirata@gmail.com>
2022-07-12 16:42:07 -0300

    index 0.1

---
+++ foot-home.php
@@ -0,0 +1,3 @@
+<script src="comp/js/quagga.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,33 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="comp/css/style.css">
-</head>
-<body>
-	<div id="resultado">
-		<a class='botao inativo'>Escaneie o código de barras</a>
+<?php include "head-home.php" ?>
+
+<div class="container-fluid">
+	<div class="row mb-4">
+		<div class="col-12 d-flex justify-content-center">
+			<div id="resultado">
+				<a class='botao inativo'>
+					Escaneie o código de barras
+				</a>
+			</div>
+		</div>
 	</div>
-	<div id="camera"></div>
-	
-	<script src="comp/js/quagga.min.js"></script>
-	<script>
+	<div class="row">
+		<div class="col-12 d-flex justify-content-center">
+			<button id="cameraStart" class="cameraStartButton" onclick="cameraStart()">
+				Aperte para começar
+			</button>
+			
+			<div id="cameraHolder"></div>
+		</div>
+	</div>
+</div>
+
+<script>
+	function orderByOcurrence(arr){
+		var counts = {};
+		arr.forEach(function(value){
+			if(!counts[value]) {
+				counts[value] = 0;
+			}
+			counts[value]++;
+		});
+		return Object.keys(counts).sort(function(curKey, nextKey){
+			return counts[curKey] < counts[nextKey];
+		});
+	}
+	function cameraStart() {
+		document.getElementById("cameraStart").classList.add ("d-none");
+		document.getElementById("cameraHolder").innerHTML = "<div id='camera'></div>";
+
 		Quagga.init({
 			inputStream : {
 				name : "Live",
 				type : "LiveStream",
-		      	target: document.querySelector('#camera'),
-		      	constraints: {
-				    width: 640,
-				    height: 300
+				target: document.querySelector('#camera'),
+				constraints: {
+					width: 640,
+					height: 300
 				}
-		  	},
-		  	decoder : {
-		  		readers : ["code_128_reader"]
-		  	}
-		}, function(err) {
+			},
+			decoder : {
+				readers : ["ean_reader"]
+			}
+		}, 
+		function(err) {
 			if (err) {
 				console.log(err);
 				return
@@ -36,13 +61,22 @@
 			Quagga.start();
 		});
 
+		var lastResult = [];
 		Quagga.onDetected(function(data){
+			var barCode;
 			var page = data.codeResult.code;
-			var link = "<a class='botao' target='_blank' href='" + page + ".html'>Clique aqui " + page + "</a>";
-			document.querySelector('#resultado').innerHTML = link;
-			console.log(link);
+			var link;
+			lastResult.push(page);
+			if (lastResult.length > 20){
+				barCode = orderByOcurrence(lastResult)[0];
+				link = "<a class='botao' target='_blank' href='" + barCode + "'>Clique aqui " + barCode + "</a>";
+				Quagga.stop();	
+				document.querySelector('#resultado').innerHTML = link;
+				console.log(link);
+			}
 		});
-	</script>	
 
-</body>
-</html>
+	}
+</script>
+
+<?php include "foot-home.php" ?>
