12f838dc30a2ce5deb20c5e928e7321f311269fe
Renato Hirata <renatokhirata@gmail.com>
2022-07-13 11:02:08 -0300

    index 0.2

--- index.php
+++ index.php
@@ -1,26 +1,45 @@
 <?php include "head-home.php" ?>
 
-<div class="container-fluid">
-	<div class="row mb-4">
-		<div class="col-12 d-flex justify-content-center">
-			<div id="resultado">
-				<a class='botao inativo'>
-					Escaneie o código de barras
-				</a>
-			</div>
-		</div>
+<div class="home">
+<div id="resultado-modal" class="resultado-modal-bg d-none">
+	<div id="resultado" class="resultado-modal-box">
+		<a class="botao">
+			Escaneie o código de barras
+		</a>
 	</div>
-	<div class="row">
+</div>
+<div class="container-fluid">
+	<div class="row py-4">
 		<div class="col-12 d-flex justify-content-center">
-			<button id="cameraStart" class="cameraStartButton" onclick="cameraStart()">
-				Aperte para começar
-			</button>
-			
-			<div id="cameraHolder"></div>
+			<h1><span style="font-weight: 800;">NIVEA</span> Scan</h1>
 		</div>
 	</div>
 </div>
 
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12 d-flex justify-content-center">
+			<div class="cameraStartButtonBg">
+				<div id="cameraStart" class="cameraStartButton" onclick="cameraStart('firstTime')">
+					<img src="comp/images/icon-barcode.svg" alt="NIVEA">
+					<h3>Aperte aqui<br>para começar</h3>
+				</div>
+				<div id="cameraHolder"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid my-4">
+	<div class="row d-flex justify-content-center">
+		<div class="col-10 text-center">
+			<h3 class="font-weight-bold">O cuidado com você começa com mais informação.</h3>
+			<p>Escaneie o código de barras dos produtos NIVEA para saber mais e descobrir qual é o ideal para a sua rotina de cuidado.</p>
+		</div>
+	</div>
+</div>
+
+</div> <!--END Home -->
 <script>
 	function orderByOcurrence(arr){
 		var counts = {};
@@ -34,18 +53,21 @@
 			return counts[curKey] < counts[nextKey];
 		});
 	}
-	function cameraStart() {
-		document.getElementById("cameraStart").classList.add ("d-none");
-		document.getElementById("cameraHolder").innerHTML = "<div id='camera'></div>";
-
+	function cameraStart(first) {
+		var isItFirstTime = first;
+		if (isItFirstTime == "firstTime") {
+			document.getElementById("cameraHolder").innerHTML = "<div id='camera'></div>";
+		} else {
+			document.getElementById("resultado-modal").classList.add("d-none");
+		}
 		Quagga.init({
 			inputStream : {
 				name : "Live",
 				type : "LiveStream",
 				target: document.querySelector('#camera'),
 				constraints: {
-					width: 640,
-					height: 300
+					width: 390,
+					height: 390
 				}
 			},
 			decoder : {
@@ -62,17 +84,23 @@
 		});
 
 		var lastResult = [];
+		var listValidEAN = ["4005900950987","42398004","4005900079664","4005900139849","4005900813046","4005900813053","4005900889720","4005900889737","4005900894779","4005900929358","4005900929679","4005900929686"]
 		Quagga.onDetected(function(data){
 			var barCode;
 			var page = data.codeResult.code;
 			var link;
 			lastResult.push(page);
-			if (lastResult.length > 20){
+			if (lastResult.length > 15){
 				barCode = orderByOcurrence(lastResult)[0];
-				link = "<a class='botao' target='_blank' href='" + barCode + "'>Clique aqui " + barCode + "</a>";
-				Quagga.stop();	
-				document.querySelector('#resultado').innerHTML = link;
-				console.log(link);
+				Quagga.stop();
+				if (listValidEAN.includes(barCode)) {
+					console.log(barCode);
+					link = "<img class='resultado-image' src='comp/images/" + barCode + "/" + barCode + "-00.png' alt='NIVEA'><a class='resultado-botao' target='_blank' href='" + barCode + "'>Ver produto</a><br><button class='resultado-botao-secondary' onclick='cameraStart()'>Escanear outro</button>";
+					document.querySelector('#resultado').innerHTML = link;
+					document.getElementById("resultado-modal").classList.remove("d-none");
+				} else {
+					console.log("vish");
+				}
 			}
 		});
 
@@ -80,3 +108,7 @@
 </script>
 
 <?php include "foot-home.php" ?>
+
+<!-- <img class='resultado-image' src='comp/images/" + barCode + "/" + barCode + "-00.png' alt='NIVEA'>
+<a class='resultado-botao' target='_blank' href='" + barCode + "'>Ver produto</a><br>
+<button class='resultado-botao-secondary' onclick='cameraStart()'>Escanear outro</button> -->
