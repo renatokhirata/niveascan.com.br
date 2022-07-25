168033b2bab64f21be291bde77e2270f7ba92050
claupinheiros <102977963+claupinheiros@users.noreply.github.com>
2022-07-25 12:51:36 -0300

    Add files via upload

---
+++ 4005808846290.php
@@ -0,0 +1,220 @@
+<?php include "head.php" ?>
+<!-- 4005808846290 -->
+<style>:root {
+	--cor01: #0033a1;
+	--cor02: white;
+	--cor03: #dcb100;
+}</style>
+
+<body>
+        <div class="container-fluid containerIntro">
+		<div class="row justify-content-center d-flex">
+			<div class="col-12 col-md-6">
+
+				<?php include "estrela.php" ?>
+
+				<h1 class="text-center">
+				<span>Tenho </span>
+                <span>36</span>
+                <span>anos,</span>
+                <span>em </span>
+                <span>5 </span>
+                <span>dias </span>
+                <span>de </span>
+                <span>uso </span>
+                <span>noturno </span>
+                <span>umas </span>
+                <span>manchinhas </span>
+                <span>que </span>
+                <span>pareciam </span>
+                <span>sardas, </span>
+                <span>estão </span>
+                <span>quase </span>
+                <span>sumindo. </span>
+                <span>E </span>
+                <span>a </span>
+                <span>pele </span>
+                <span>está </span>
+                <span>mais </span>
+                <span>"aveludada".</span>	
+				
+            </h1>
+				<p class="introAutor text-center">
+				<b>
+                Carla
+				</b><br>
+                13/01/2022 • NIVEA
+				</p>
+				<?php include "seta.php"?>
+			</div>
+		</div>
+	</div>
+
+	<div id="second"></div>
+	<div class="container-fluid containerA">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 d-flex justify-content-center">
+			<picture>
+    <source type="image/webp" srcset="comp/images/logo-nivea.webp">
+    <source type="image/jpeg" srcset="comp/images/logo-nivea.png">	
+			<img src="comp/images/logo-nivea.png" class="nivea-logo" alt="NIVEA">
+</picture>
+			</div>
+			<div class="col-8 col-md-4">
+			<picture>
+    <source type="image/webp" srcset="comp/images/4005808846290/4005808846290_00.webp">
+    <source type="image/jpeg" srcset="comp/images/4005808846290/4005808846290_00.png">	
+			<img src="comp/images/4005808846290/4005808846290_00.png" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+</picture>
+			</div>
+			<div class="col-12 col-md-9 text-center">
+				<h1 class="fade-h1 fade-revealUp">
+                Creme para as Mãos Q10 Plus Reparação 
+					<br><span>75g</span>
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid containerB">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-12">
+				<div id="prodCarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<div class="carousel-imgctn">
+							<picture>
+    <source type="image/webp" srcset="comp/images/4005808846290/4005808846290_3.webp">
+    <source type="image/jpeg" srcset="comp/images/4005808846290/4005808846290_3.jpg">	
+							<img src="comp/images/4005808846290/4005808846290_3.jpg" alt="NIVEA">
+</picture>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<div class="carousel-imgctn">
+							<picture>
+    <source type="image/webp" srcset="comp/images/4005808846290/4005808846290_4.webp">
+    <source type="image/jpeg" srcset="comp/images/4005808846290/4005808846290_4.jpg">	
+							<img src="comp/images/4005808846290/4005808846290_4.jpg" alt="NIVEA">
+</picture>
+							</div>
+						</div>
+                        <div class="carousel-item">
+							<div class="carousel-imgctn">
+							<picture>
+    <source type="image/webp" srcset="comp/images/4005808846290/4005808846290_5.webp">
+    <source type="image/jpeg" srcset="comp/images/4005808846290/4005808846290_5.jpg">	
+							<img src="comp/images/4005808846290/4005808846290_5.jpg" alt="NIVEA">
+</picture>
+							</div>
+						</div>
+                        <div class="carousel-item">
+							<div class="carousel-imgctn">
+							<picture>
+    <source type="image/webp" srcset="comp/images/4005808846290/4005808846290_6.webp">
+    <source type="image/jpeg" srcset="comp/images/4005808846290/4005808846290_6.jpg">	
+							<img src="comp/images/4005808846290/4005808846290_6.jpg" alt="NIVEA">
+</picture>
+							</div>
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-target="#prodCarousel" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					</button>
+					<button class="carousel-control-next" type="button" data-target="#prodCarousel" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerTexto">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h3 class="fade-revealUp">
+					O que é?
+				</h3>
+				<p class="fade-revealUp">
+                <b>Creme para as Mãos NIVEA Q10 Plus</b> hidrata, previne envelhecimento e reduz sinais da idade. Garanta o seu e cuide bem da sua pele!
+				</p>
+				<h3 class="fade-revealUp">
+                Para que serve?
+				</h3>
+				<p class="fade-revealUp">
+                Para todos os tipos de pele. Sua fórmula com Q10 Plus, coenzima produzida naturalmente no corpo, estimula a renovação celular.
+				</p>
+				<h3 class="fade-revealUp">
+					Benefícios e diferenciais
+				</h3>
+				<div class="beneficios">
+                    <div class="fade-revealUp">
+                    Hidratação por 24h
+					</div>
+                    <div class="fade-revealUp">
+                    Q10 Plus previne envelhecimento
+					</div>
+                    <div class="fade-revealUp">
+                    Antissinais e Anti-idade
+					</div>
+                    <div class="fade-revealUp">
+                    Dermatologicamente testado
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerC">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 text-center">
+				<h1>Modo de uso</h1>
+				<div class="d-block d-md-none">
+					<div class="modoUsoDiv" style="background-image: url('comp/images/4005808846290/4005808846290_7.jpg')">
+					</div>
+				</div>
+				<div class="d-none d-md-block">
+					<div class="modoUsoDiv-desktop" style="background-image: url('comp/images/4005808846290/4005808846290_7.jpg')">
+					</div>
+				</div>				
+				<h3 id="ingredientes" class="ingredientes" onclick="ingredientesClick()">Ingredientes &#x25BE</h3>
+				<div id="ingredientesLista" class="ingredientesLista">
+					<p>
+                    Aqua, Glycerin, Octyldodecanol, Cetyl Alcohol, Octocrylene, Stearyl Alcohol, Glyceryl Stearate SE, Hydrogenated Coco-Glycerides, Creatine, 1-Methylhydantoin-2-Imide, Ubiquinone, Butyrospermum Parkii Butter, Dimethicone, Butyl Methoxydibenzoylmethane, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Carbomer, Sodium Cetearyl Sulfate, Sodium Hydroxide, Ethylhexylglycerin, Phenoxyethanol, Trisodium EDTA, Linalool, Citronellol, Hexyl Cinnamal, Benzyl Alcohol, Alpha-Isomethyl Ionone, d-Limonene, Eugenol, Parfum
+                    </div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerD">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h1 class="fade-revealUp">Especificações</h1>
+				<picture>
+    <source type="image/webp" srcset="comp/images/4005808846290/4005808846290_8.webp">
+    <source type="image/jpeg" srcset="comp/images/4005808846290/4005808846290_8.jpg">
+				<img src="comp/images/4005808846290/4005808846290_8.jpg" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+</picture>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerE">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-10 text-center">
+				<video width="100%" height="auto" controls autoplay muted loop>
+					<source src="comp/images/4005808846290/4005808846290_video.mp4" type="video/mp4">
+						Your browser does not support the video tag.
+					</video>
+				</div>
+			</div>
+		</div> 
+
+		<div class="container-fluid containerF">
+			<div class="row">
+				<div class="col-12">
+					<h1>Aproveite</h1>
+				</div>
+			</div>
+		</div>
+
+<?php include "foot.php" ?>
