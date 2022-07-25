cf38d521de55f90c614c98baeeff480a1da10ca2
claupinheiros <102977963+claupinheiros@users.noreply.github.com>
2022-07-25 17:27:01 -0300

    Add files via upload

---
+++ 4005900850041.php
@@ -0,0 +1,203 @@
+<?php include "head.php" ?>
+<!-- 4005900704337 -->
+<style>:root {
+	--cor01: #0033a1;
+	--cor02: white;
+	--cor03: #002981
+}</style>
+
+<body>
+        <div class="container-fluid containerIntro">
+		<div class="row justify-content-center d-flex">
+			<div class="col-12 col-md-6">
+
+				<?php include "estrela.php" ?>
+
+				<h1 class="text-center">
+				<span>Altamente </span>
+                <span>hidratante, </span>
+                <span>cheiro </span>
+                <span>suave, </span>
+                <span>realmente </span>
+                <span>cuida </span>
+                <span>da </span>
+                <span>pele </span>
+                <span>seca </span>
+                <span>e </span>
+                <span>alivia </span>
+                <span>as </span>
+                <span>assaduras </span>
+                <span>por </span>
+                <span>atrito.</span>	
+				
+            </h1>
+				<p class="introAutor text-center">
+				<b>
+                Silvia P
+				</b><br>
+                22/01/2022 • NIVEA
+				</p>
+				<?php include "seta.php"?>
+			</div>
+		</div>
+	</div>
+
+	<div id="second"></div>
+	<div class="container-fluid containerA">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 d-flex justify-content-center">
+			<picture>
+    <source type="image/webp" srcset="comp/images/logo-nivea.webp">
+    <source type="image/jpeg" srcset="comp/images/logo-nivea.png">	
+			<img src="comp/images/logo-nivea.png" class="nivea-logo" alt="NIVEA">
+</picture>
+			</div>
+			<div class="col-8 col-md-4">
+			<picture>
+    <source type="image/webp" srcset="comp/images/4005900704337/4005900704337_00.webp">
+    <source type="image/jpeg" srcset="comp/images/4005900704337/4005900704337_00.png">	
+			<img src="comp/images/4005900704337/4005900704337_00.png" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+</picture>
+			</div>
+			<div class="col-12 col-md-9 text-center">
+				<h1 class="fade-h1 fade-revealUp">
+                Creme Hidratante Arco-Íris Edição Limitada Orgulho 
+					<br><span>56g</span>
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid containerB">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-12">
+				<div id="prodCarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<div class="carousel-imgctn">
+							<picture>
+    <source type="image/webp" srcset="comp/images/4005900704337/4005900704337_3.webp">
+    <source type="image/jpeg" srcset="comp/images/4005900704337/4005900704337_3.jpg">	
+							<img src="comp/images/4005900704337/4005900704337_3.jpg" alt="NIVEA">
+</picture>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<div class="carousel-imgctn">
+							<picture>
+    <source type="image/webp" srcset="comp/images/4005900704337/4005900704337_4.webp">
+    <source type="image/jpeg" srcset="comp/images/4005900704337/4005900704337_4.jpg">	
+							<img src="comp/images/4005900704337/4005900704337_4.jpg" alt="NIVEA">
+</picture>
+							</div>
+						</div>
+                        <div class="carousel-item">
+							<div class="carousel-imgctn">
+							<picture>
+    <source type="image/webp" srcset="comp/images/4005900704337/4005900704337_5.webp">
+    <source type="image/jpeg" srcset="comp/images/4005900704337/4005900704337_5.jpg">	
+							<img src="comp/images/4005900704337/4005900704337_5.jpg" alt="NIVEA">
+</picture>
+							</div>
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-target="#prodCarousel" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					</button>
+					<button class="carousel-control-next" type="button" data-target="#prodCarousel" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerTexto">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h3 class="fade-revealUp">
+					O que é?
+				</h3>
+				<p class="fade-revealUp">
+                A <b>NIVEA</b> agora está ainda mais representativa. Nesta edição limitada, o creme mais tradicional do mundo ganha uma versão #Pride. Sinta o orgulho na pele!
+				</p>
+				<h3 class="fade-revealUp">
+                Para que serve?
+				</h3>
+				<p class="fade-revealUp">
+                Hidrata especialmente áreas mais ressecadas do corpo como cotovelo, calcanhar, joelho, mãos e pés. Com glicerina e Eucerit<sup>®</sup>, mantém o toque suave na pele.
+				</p>
+				<h3 class="fade-revealUp">
+					Benefícios e diferenciais
+				</h3>
+				<div class="beneficios">
+                    <div class="fade-revealUp">
+                    Protege do frio
+					</div>
+                    <div class="fade-revealUp">
+                    Uniformiza o tom da pele em 2 semanas*
+					</div>
+                    <div class="fade-revealUp">
+                    Hidratante pós-barba
+					</div>
+                    <div class="fade-revealUp">
+                    Pode usar para fazer massagens
+					</div>
+                    <div class="fade-revealUp">
+                    Cuida da pele tatuada e muito mais
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid containerC">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 text-center">
+				<h1>Modo de uso</h1>
+				<div class="d-block d-md-none">
+					<div class="modoUsoDiv" style="background-image: url('comp/images/4005900704337/40059007043376.jpg')">
+					</div>
+				</div>
+				<div class="d-none d-md-block">
+					<div class="modoUsoDiv-desktop" style="background-image: url('comp/images/4005900704337/4005900704337_6.jpg')">
+					</div>
+				</div>				
+				<h3 id="ingredientes" class="ingredientes" onclick="ingredientesClick()">Ingredientes &#x25BE</h3>
+				<div id="ingredientesLista" class="ingredientesLista">
+					<p>
+                    Aqua, Paraffinum Liquidum, Cera Microcristallina,Glycerin, Lanolin Alcohol (Eucerit®), Paraffin, Panthenol, Magnesium Sulfate, Decyl Oleate, Octyldodecanol, Aluminum Stearates, Citric Acid, Magnesium Stearate, Sodium Anisate, d-Limonene, Geraniol, Hydroxycitronellal, Linalool, Citronellol, Benzyl Benzoate, Cinnamyl Alcohol, Parfum
+	</div>
+
+	<div class="container-fluid containerD">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-6 text-center">
+				<h1 class="fade-revealUp">Especificações</h1>
+				<picture>
+    <source type="image/webp" srcset="comp/images/4005900704337/4005900704337_7.webp">
+    <source type="image/jpeg" srcset="comp/images/4005900704337/4005900704337_7.jpg">
+				<img src="comp/images/4005900704337/4005900704337_7.jpg" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+</picture>
+			</div>
+		</div>
+	</div>
+    <!--
+	<div class="container-fluid containerE">
+		<div class="row d-flex justify-content-center">
+			<div class="col-12 col-md-10 text-center">
+				<video width="100%" height="auto" controls autoplay muted loop>
+					<source src="comp/images/4005900704337/4005900704337_video.mp4" type="video/mp4">
+						Your browser does not support the video tag.
+					</video>
+				</div>
+			</div>
+		</div> 
+-->
+		<div class="container-fluid containerF">
+			<div class="row">
+				<div class="col-12">
+					<h1>Compre o seu</h1>
+				</div>
+			</div>
+		</div>
+
+<?php include "foot.php" ?>
