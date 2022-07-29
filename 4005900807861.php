4b54670c7472c11c2bbc73c70630753008545073
claupinheiros <102977963+claupinheiros@users.noreply.github.com>
2022-07-29 17:02:18 -0300

    Add files via upload

---
+++ 4005900807861.php
@@ -0,0 +1,202 @@
+<?php include "head.php" ?>
+<!-- 4005900807861 -->
+<style>:root {
+	--cor01: #0033a1;
+	--cor02: white;
+	--cor03: #4685a8;
+    }
+</style>
+
+<body>
+    <div class="container-fluid containerIntro d-none">
+		<div class="row justify-content-center d-flex">
+			<div class="col-12 col-md-6">
+
+				<?php include "estrela.php" ?>
+
+				<h1 class="text-center">
+				<span>Um </span>
+				</h1>
+
+				<p class="introAutor text-center">
+				<b>
+                Camila R
+				</b><br>
+                09/06/2022 • NIVEA
+				</p>
+				<?php include "seta.php"?>
+			</div>
+		</div>
+	</div>
+
+	<div id="second"></div>
+	    <div class="container-fluid containerA">
+		    <div class="row d-flex justify-content-center">
+			   <div class="col-12 d-flex justify-content-center">
+			        <picture>
+                        <source type="image/webp" srcset="comp/images/logo-nivea.webp">
+                            <source type="image/jpeg" srcset="comp/images/logo-nivea.png">	
+			                    <img src="comp/images/logo-nivea.png" class="nivea-logo" alt="NIVEA">
+                    </picture>
+			   </div>
+			   <div class="col-8 col-md-4">
+			        <picture>
+                        <source type="image/webp" srcset="comp/images/4005900807861/4005900807861_00.webp">
+                             <source type="image/jpeg" srcset="comp/images/4005900807861/4005900807861_00.png">	
+			                    <img src="comp/images/4005900807861/4005900807861_00.png" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+                   </picture>
+			   </div>
+			   <div class="col-12 col-md-9 text-center">
+				   <h1 class="fade-h1 fade-revealUp">
+                   Sabonete Pure Milk Beauty Elixir Fresh
+					   <br><span>90g</span>
+				   </h1>
+			   </div>
+	     </div>
+	 </div>
+
+	    <div class="container-fluid containerB">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-12">
+				    <div id="prodCarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
+					    <div class="carousel-inner">
+						    <div class="carousel-item active">
+							    <div class="carousel-imgctn">
+							        <picture>
+                                        <source type="image/webp" srcset="comp/images/4005900807861/4005900807861_3.webp">
+                                           <source type="image/jpeg" srcset="comp/images/4005900807861/4005900807861_3.jpg">	
+							                    <img src="comp/images/4005900807861/4005900807861_3.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div>
+						    <div class="carousel-item">
+							    <div class="carousel-imgctn">
+							        <picture>
+                                       <source type="image/webp" srcset="comp/images/4005900807861/4005900807861_4.webp">
+                                          <source type="image/jpeg" srcset="comp/images/4005900807861/4005900807861_4.jpg">	
+							                    <img src="comp/images/4005900807861/4005900807861_4.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div>
+                            <div class="carousel-item">
+							    <div class="carousel-imgctn">
+							        <picture>
+                                        <source type="image/webp" srcset="comp/images/4005900807861/4005900807861_5.webp">
+                                            <source type="image/jpeg" srcset="comp/images/4005900807861/4005900807861_5.jpg">	
+							                    <img src="comp/images/4005900807861/4005900807861_5.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div> 
+ <!--                                                                                                                                                                     
+                            <div class="carousel-item">
+							    <div class="carousel-imgctn">
+							       <picture>
+                                       <source type="image/webp" srcset="comp/images/4005900807861/4005900807861_6.webp">
+                                       <source type="image/jpeg" srcset="comp/images/4005900807861/4005900807861_6.jpg">	
+							                    <img src="comp/images/4005900807861/4005900807861_6.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div>   
+-->                                                                                                                                                                                
+					    </div>
+					    <button class="carousel-control-prev" type="button" data-target="#prodCarousel" data-slide="prev">
+						   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					    </button>
+					    <button class="carousel-control-next" type="button" data-target="#prodCarousel" data-slide="next">
+						   <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					    </button>
+				    </div>
+			    </div>
+		    </div>
+	    </div>
+
+	    <div class="container-fluid containerTexto">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-6 text-center">
+				    <h3 class="fade-revealUp">
+				         O que é?
+				    </h3>
+				    <p class="fade-revealUp">
+                    Sabonete hidratante que oferece cuidado, proteção e de renovação da pele todos os dias. Cuidado único de <b>NIVEA</b> para você. Aproveite!
+				    </p>
+				   <h3 class="fade-revealUp">
+                         Para que serve?
+				   </h3>
+				   <p class="fade-revealUp">
+                   Fórmula enriquecida com a preciosa essência do leite e deliciosa fragrância, proporciona uma pele macia e hidratada mesmo após o banho.
+				   </p>
+				   <h3 class="fade-revealUp">
+				         Benefícios e diferenciais
+				   </h3>
+				   <div class="beneficios">
+                        <div class="fade-revealUp">
+                        Fórmula especial hidrata a pele
+				        </div>
+                        <div class="fade-revealUp">
+                        Mais refrescância na versão Fresh
+                        </div>
+                        <div class="fade-revealUp">
+                        Sabonete glicerinado
+				        </div>
+                        <div class="fade-revealUp">
+                        Dermatologicamente testado
+				        </div>
+				    </div>
+			    </div>
+		    </div>
+	    </div>
+
+	    <div class="container-fluid containerC">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 text-center">
+				     <h1>Modo de uso</h1>
+				     <div class="d-block d-md-none">
+					    <div class="modoUsoDiv" style="background-image: url('comp/images/4005900807861/4005900807861_7.jpg')">
+					    </div>
+				    </div>
+				    <div class="d-none d-md-block">
+					    <div class="modoUsoDiv-desktop" style="background-image: url('comp/images/4005900807861/4005900807861_7.jpg')">
+					    </div>
+				    </div>				
+				    <h3 id="ingredientes" class="ingredientes" onclick="ingredientesClick()">Ingredientes &#x25BE</h3>
+				    <div id="ingredientesLista" class="ingredientesLista">
+					   <p>
+                       Sodium Oleate, Sodium Stearate, Sodium Palmitate, Aqua, Zea Mays Starch, Glycerin, Sodium Myristate, CI 77891, Sodium Linoleate, Sodium Laurate | Talc, Lactose, Calcium Pantothenate, Tocopheryl Acetate, BHT, Sodium Chloride, Citric Acid, Tetrasodium EDTA, Etidronic Acid, Benzyl Alcohol, Citronellol, Geraniol, Parfum
+                    </div>
+			    </div>
+		    </div>
+	    </div>
+
+	    <div class="container-fluid containerD">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-6 text-center">
+				    <h1 class="fade-revealUp">Especificações</h1>
+				    <picture>
+                       <source type="image/webp" srcset="comp/images/4005900807861/4005900807861_8.webp">
+                           <source type="image/jpeg" srcset="comp/images/4005900807861/4005900807861_8.jpg">
+				               <img src="comp/images/4005900807861/4005900807861_8.jpg" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+                    </picture>
+			    </div>
+		    </div>
+	    </div>
+<!--
+	    <div class="container-fluid containerE">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-10 text-center">
+				    <video width="100%" height="auto" controls autoplay muted loop>
+					    <source src="comp/images/4005900807861/4005900807861_video.mp4" type="video/mp4">
+						    Your browser does not support the video tag.
+			    	</video>
+				</div>
+			</div>
+		</div> 
+-->
+		<div class="container-fluid containerF">
+			<div class="row">
+				<div class="col-12">
+					<h1>Aproveite</h1>
+				</div>
+			</div>
+		</div>
+
+<?php include "foot.php" ?>
---
+++ 4005900909800.php
@@ -0,0 +1,202 @@
+<?php include "head.php" ?>
+<!-- 4005900909800 -->
+<style>:root {
+	--cor01: #0033a1;
+	--cor02: white;
+	--cor03: #00a51b;
+    }
+</style>
+
+<body>
+    <div class="container-fluid containerIntro d-none">
+		<div class="row justify-content-center d-flex">
+			<div class="col-12 col-md-6">
+
+				<?php include "estrela.php" ?>
+
+				<h1 class="text-center">
+				<span>Um </span>
+				</h1>
+
+				<p class="introAutor text-center">
+				<b>
+                Camila R
+				</b><br>
+                09/06/2022 • NIVEA
+				</p>
+				<?php include "seta.php"?>
+			</div>
+		</div>
+	</div>
+
+	<div id="second"></div>
+	    <div class="container-fluid containerA">
+		    <div class="row d-flex justify-content-center">
+			   <div class="col-12 d-flex justify-content-center">
+			        <picture>
+                        <source type="image/webp" srcset="comp/images/logo-nivea.webp">
+                            <source type="image/jpeg" srcset="comp/images/logo-nivea.png">	
+			                    <img src="comp/images/logo-nivea.png" class="nivea-logo" alt="NIVEA">
+                    </picture>
+			   </div>
+			   <div class="col-8 col-md-4">
+			        <picture>
+                        <source type="image/webp" srcset="comp/images/4005900909800/4005900909800_00.webp">
+                             <source type="image/jpeg" srcset="comp/images/4005900909800/4005900909800_00.png">	
+			                    <img src="comp/images/4005900909800/4005900909800_00.png" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+                   </picture>
+			   </div>
+			   <div class="col-12 col-md-9 text-center">
+				   <h1 class="fade-h1 fade-revealUp">
+                   Sabonete em barra Natural e Essencial Aloe e Vera
+					   <br><span>90g</span>
+				   </h1>
+			   </div>
+	     </div>
+	 </div>
+
+	    <div class="container-fluid containerB">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-12">
+				    <div id="prodCarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
+					    <div class="carousel-inner">
+						    <div class="carousel-item active">
+							    <div class="carousel-imgctn">
+							        <picture>
+                                        <source type="image/webp" srcset="comp/images/4005900909800/4005900909800_3.webp">
+                                           <source type="image/jpeg" srcset="comp/images/4005900909800/4005900909800_3.jpg">	
+							                    <img src="comp/images/4005900909800/4005900909800_3.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div>
+						    <div class="carousel-item">
+							    <div class="carousel-imgctn">
+							        <picture>
+                                       <source type="image/webp" srcset="comp/images/4005900909800/4005900909800_4.webp">
+                                          <source type="image/jpeg" srcset="comp/images/4005900909800/4005900909800_4.jpg">	
+							                    <img src="comp/images/4005900909800/4005900909800_4.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div>
+                            <div class="carousel-item">
+							    <div class="carousel-imgctn">
+							        <picture>
+                                        <source type="image/webp" srcset="comp/images/4005900909800/4005900909800_5.webp">
+                                            <source type="image/jpeg" srcset="comp/images/4005900909800/4005900909800_5.jpg">	
+							                    <img src="comp/images/4005900909800/4005900909800_5.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div> 
+ <!--                                                                                                                                                                     
+                            <div class="carousel-item">
+							    <div class="carousel-imgctn">
+							       <picture>
+                                       <source type="image/webp" srcset="comp/images/4005900909800/4005900909800_6.webp">
+                                       <source type="image/jpeg" srcset="comp/images/4005900909800/4005900909800_6.jpg">	
+							                    <img src="comp/images/4005900909800/4005900909800_6.jpg" alt="NIVEA">
+                                    </picture>
+							    </div>
+						    </div>   
+-->                                                                                                                                                                                
+					    </div>
+					    <button class="carousel-control-prev" type="button" data-target="#prodCarousel" data-slide="prev">
+						   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					    </button>
+					    <button class="carousel-control-next" type="button" data-target="#prodCarousel" data-slide="next">
+						   <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					    </button>
+				    </div>
+			    </div>
+		    </div>
+	    </div>
+
+	    <div class="container-fluid containerTexto">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-6 text-center">
+				    <h3 class="fade-revealUp">
+				         O que é?
+				    </h3>
+				    <p class="fade-revealUp">
+                    O novo <b>Sabonete Natural & Essencial Aloe Vera</b> é produzido com 99% de ingredientes naturais. Fórmula vegana que cuida de você e do planeta. 
+				    </p>
+				   <h3 class="fade-revealUp">
+                         Para que serve?
+				   </h3>
+				   <p class="fade-revealUp">
+                   Além da sua fórmula com hidratante que limpa e refresca a pele, o <b>NIVEA Sabonete Aloe Vera</b> tem embalagem produzida com papel cartão feito 100% de fonte renovável. É limpeza da pele junto ao cuidado com o planeta. 
+				   </p>
+				   <h3 class="fade-revealUp">
+				         Benefícios e diferenciais
+				   </h3>
+				   <div class="beneficios">
+                        <div class="fade-revealUp">
+                        Fragrância de Aloe Vera
+				        </div>
+                        <div class="fade-revealUp">
+                        Fórmula vegana
+                        </div>
+                        <div class="fade-revealUp">
+                        0% de ingredientes de origem animal
+				        </div>
+                        <div class="fade-revealUp">
+                        Limpa e cuida da sua pele
+				        </div>
+				    </div>
+			    </div>
+		    </div>
+	    </div>
+
+	    <div class="container-fluid containerC">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 text-center">
+				     <h1>Modo de uso</h1>
+				     <div class="d-block d-md-none">
+					    <div class="modoUsoDiv" style="background-image: url('comp/images/4005900909800/4005900909800_7.jpg')">
+					    </div>
+				    </div>
+				    <div class="d-none d-md-block">
+					    <div class="modoUsoDiv-desktop" style="background-image: url('comp/images/4005900909800/4005900909800_7.jpg')">
+					    </div>
+				    </div>				
+				    <h3 id="ingredientes" class="ingredientes" onclick="ingredientesClick()">Ingredientes &#x25BE</h3>
+				    <div id="ingredientesLista" class="ingredientesLista">
+					   <p>
+                       Sodium Palmitate (Palmitato de Sódio), Sodium Oleate (Oleato de Sódio), Aqua (Água), Glycerin (Glicerol), Sodium Linoleate (Linoleato de Sódio), Sodium Laurate (Laurato de Sódio), Sodium Stearate (Estearato de Sódio), Zea Mays Starch (Amido), Sodium Myristate (Miristato de Sódio), Aloe Barbadensis Leaf Juice Powder (Suco da Folha de Aloe Barbadensis em Pó), Citric Acid (Ácido Cítrico), Sodium Chloride (Cloreto de Sódio), , Tetrasodium EDTA (Edetato de Sódio), Etidronic Acid (Ácido Etidrônico), Sodium Polynaphthalenesulfonate (Polinaftalenossulfonato de Sódio), Parfum (Perfume), CI 77891 (Dióxido de Titânio), CI 77492 (Óxido de Ferro Amarelo), CI 74160 (Corante Azul 74160)
+                    </div>
+			    </div>
+		    </div>
+	    </div>
+
+	    <div class="container-fluid containerD">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-6 text-center">
+				    <h1 class="fade-revealUp">Especificações</h1>
+				    <picture>
+                       <source type="image/webp" srcset="comp/images/4005900909800/4005900909800_8.webp">
+                           <source type="image/jpeg" srcset="comp/images/4005900909800/4005900909800_8.jpg">
+				               <img src="comp/images/4005900909800/4005900909800_8.jpg" style="max-width: 100%;" alt="NIVEA" class="fade-revealUp">
+                    </picture>
+			    </div>
+		    </div>
+	    </div>
+<!--
+	    <div class="container-fluid containerE">
+		    <div class="row d-flex justify-content-center">
+			    <div class="col-12 col-md-10 text-center">
+				    <video width="100%" height="auto" controls autoplay muted loop>
+					    <source src="comp/images/4005900909800/4005900909800_video.mp4" type="video/mp4">
+						    Your browser does not support the video tag.
+			    	</video>
+				</div>
+			</div>
+		</div> 
+-->
+		<div class="container-fluid containerF">
+			<div class="row">
+				<div class="col-12">
+					<h1>Aproveite</h1>
+				</div>
+			</div>
+		</div>
+
+<?php include "foot.php" ?>
